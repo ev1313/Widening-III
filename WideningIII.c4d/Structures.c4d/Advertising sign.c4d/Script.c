f317afd28b43dd6e566a02a8d1710132802fa952
Tim Blume <tim2.blume@web.de>
2014-09-13 09:41:42 +0200

    tidy up of the advertising sign

--- WideningIII.c4d/Structures.c4d/Advertising sign.c4d/Script.c
+++ WideningIII.c4d/Structures.c4d/Advertising sign.c4d/Script.c
@@ -1,25 +1,20 @@
 // MSC-Script //
 
-#strict
+#strict 2
+
+#include BAS4
 
 local cash; //Auszahlung
 
 protected func Construction()
 {
-  if(FindObjectOwner (GetID(),GetOwner()))
+  if(FindObjectOwner(GetID(),GetOwner()))
   {
    Message("Wir haben bereits eine Werbetafel.",GetCursor(GetOwner()) );
    return(RemoveObject());
   }
 
-  SetLocal(9,CreateObject(BAS4,0,+8,GetOwner()));
   SetAction("Stop");
-  return(1);
-}
-  
-protected func Destruction()
-{
-  if (Local(9)) RemoveObject(Local(9));
   return(1);
 }
 
@@ -42,75 +37,72 @@ protected func ControlDigDouble(caller)
 
 func Menu()
 {
- var c = Par(0);
- var m = GetWealth(GetOwner());
+  var c = Par(0);
+  var m = GetWealth(GetOwner());
 
- if(c == WE01)
- {
-  cash = 8;
-  SetWealth(GetOwner(),m-50);
-  SetAction("Mercedes");
-  Message("Vertrag wird abgeschlossen: 50",this());
-  Sound("cash2");
- }
+  if(c == WE01)
+  {
+    cash = 8;
+    SetWealth(GetOwner(),m-50);
+    SetAction("Mercedes");
+    Message("Vertrag wird abgeschlossen: 50",this());
+    Sound("cash2");
+  }
 
- if(c == WE02)
- {
-  cash = 10;
-  SetWealth(GetOwner(),m-35);
-  SetAction("Opel");
-  Message("Vertrag wird abgeschlossen: 35",this());
-  Sound("cash2");
- }
+  if(c == WE02)
+  {
+    cash = 10;
+    SetWealth(GetOwner(),m-35);
+    SetAction("Opel");
+    Message("Vertrag wird abgeschlossen: 35",this());
+    Sound("cash2");
+  }
 
- if(c == WE03)
- {
-  cash = 12;
-  SetWealth(GetOwner(),m-40);
-  SetAction("DEA");
-  Message("Vertrag wird abgeschlossen: 40",this());
-  Sound("cash2");
- }
+  if(c == WE03)
+  {
+    cash = 12;
+    SetWealth(GetOwner(),m-40);
+    SetAction("DEA");
+    Message("Vertrag wird abgeschlossen: 40",this());
+    Sound("cash2");
+  }
 
+  if(c == WE04)
+  {
+    cash = 15;
+    SetWealth(GetOwner(),m-50);
+    SetAction("SHELL");
+    Message("Vertrag wird abgeschlossen: 50",this());
+    Sound("cash2");
+  }
 
- if(c == WE04)
- {
-  cash = 15;
-  SetWealth(GetOwner(),m-50);
-  SetAction("SHELL");
-  Message("Vertrag wird abgeschlossen: 50",this());
-  Sound("cash2");
- }
+  if(c == WE05)
+  {
+    cash = 20;
+    SetWealth(GetOwner(),m-65);
+    SetAction("Signum");
+    Message("Vertrag wird abgeschlossen: 65",this());
+    Sound("cash2");
+  }
 
- if(c == WE05)
- {
-  cash = 20;
-  SetWealth(GetOwner(),m-65);
-  SetAction("Signum");
-  Message("Vertrag wird abgeschlossen: 65",this());
-  Sound("cash2");
- }
-
- if(c == WE06)
- {
-  Sound("HGfinal");
-  cash = 0;
-  SetAction("Stop");
-  Message("Transaktion eingestellt",this());
- }
+  if(c == WE06)
+  {
+    Sound("HGfinal");
+    cash = 0;
+    SetAction("Stop");
+    Message("Transaktion eingestellt",this());
+  }
 
   return(1);
 }    
 
 func PayOut()
 {
- SetWealth(GetOwner(),GetWealth(GetOwner())+cash);
- Sound("cash2");
- Message("Geld überwiesen: %d",this(), cash );
- CreateObject(DOLL,0,-40);
- return(1);
+  SetWealth(GetOwner(),GetWealth(GetOwner())+cash);
+  Sound("cash2");
+  Message("Geld überwiesen: %d",this(), cash );
+  CreateObject(DOLL,0,-40);
+  return(1);
 }
 
-// MSC-Script //
-
-public GetResearchBase: return(FNDR);  
+public func GetResearchBase() { return(FNDR); }
