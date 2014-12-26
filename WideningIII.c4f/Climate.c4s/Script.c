6545fffb0081c961204e4e2dd3e0e63c0dcbf3f5
Tim Blume <tim2.blume@web.de>
2014-12-26 13:19:42 +0100

    + added Chooser to the Climate scenario. This is only temporary, until we get a better chooser…

---
+++ WideningIII.c4f/Climate.c4s/Script.c
@@ -0,0 +1,19 @@
+/* Säurewelt */
+
+#strict 2
+
+#include CHIC
+
+func Initialize()
+{
+  Schedule("OpenMenu()", 35);
+  return true;
+}
+
+func OpenMenu()
+{
+  if(GetPlayerCount() > 1)
+    InitializeChooser([GLDM, MELE, OILP, SDOP, EXTR], [[VALG, 150], [MONE, 150]]);
+  else
+    InitializeChooser([GLDM, OILP, SDOP, EXTR], [[VALG, 150], [MONE, 150]]);
+}
